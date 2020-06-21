944f81517ea4371ad3b393889e9625c1686e2a5b
Justin Hager <coachjustinhager@gmail.com>
2020-06-21 08:12:06 -0700

    Bring up to statlessness section

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 
 //creating the root route
@@ -8,11 +11,25 @@ app.get('/',(req, res) => {
     res.render('index');
 });
 
+//creating the flashcards app route
 app.get('/cards',(req, res) => {
-    res.render('card', {prompt: "America was founded in what year?"});
+    // res.locals.prompt = cardContent.prompt[randomCard];
+    res.render('card', {prompt: `Discuss 'the blue flu'`, hint: `Atlanta cop, felony murder trial, taser`});  
+});
+
+//creating the hello form route
+app.get('/hello',(req, res) => {
+    res.render('hello');
+    
+});
+
+//creating post route for hello
+app.post('/hello',(req, res) => {
+    res.render('hello', {name: req.body.username})
+    
 });
 
 //setup development server (port 3000)
 app.listen(port, () => {
-    console.log('The app is running on localhost:3000 ✅');
+    console.log(`The app is running on localhost: ${port} ✅`);
 });
